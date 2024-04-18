7b4e7dc7f35c69da9d37a1e269a5716531e8b3e2
jirayuwat12 <jirayuwat12@gmail.com>
2024-04-18 10:00:16 +0700

    upload code and slide from class

--- final_project/etl/main.py
+++
@@ -1,9 +0,0 @@
-import time
-from kafka import KafkaProducer
-
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-
-while True:
-    current_time_str = f'current time: {time.time()}'
-    producer.send('test', current_time_str.encode())
-    time.sleep(1)
